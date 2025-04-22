0a0de563db125918fe3f491a4a8512140f19186c
Yazan Khwaireh <dark_angle.2010@hotmail.com>
2025-04-23 00:57:45 +0300

    adding and channing the path

--- todo-app/backend/src/frontend/frontend.controller.ts
+++ todo-app/backend/src/frontend/frontend.controller.ts
@@ -1,11 +1,11 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller, Get, Res, Param } from '@nestjs/common';
 import { Response } from 'express';
 import { join } from 'path';
 
 @Controller()
 export class FrontendController {
-  @Get('*')
-  serveClient(@Res() res: Response) {
+  @Get('*path')
+  serveClient(@Res() res: Response, @Param('path') path: string) {
     return res.sendFile(join(__dirname, '..', 'public', 'index.html'));
   }
 }
